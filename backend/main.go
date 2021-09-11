a7a5d4b0cf0833cdd6ebff5dc527eb7f86bdd7a6
daichi_yamaoka <tnychor06@gmail.com>
2021-09-12 00:17:49 +0900

    add docker-compose to use mysql container

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type ResponseString string
+
+func (rs ResponseString) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, rs)
+}
 
 func main() {
 	fmt.Println("starting...")
+	http.Handle("/api", ResponseString("Hello World!"))
+	http.ListenAndServe(":8080", nil)
 }
